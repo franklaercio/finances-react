913e002107bd48b46cc126b1d0d52aa53323e131
Frank Laercio <franklaercio@gmail.com>
2021-03-20 18:41:58 -0300

    Adding style on select button

--- web/src/components/NewTransactionModal/index.tsx
+++ web/src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,10 @@
 import Modal from 'react-modal';
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg'; 
 import closeImg from '../../assets/close.svg';
+import { useState } from 'react';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -11,6 +12,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+    const [type, setType] = useState('deposit'); 
+
     return (
         <Modal 
             isOpen={isOpen} 
@@ -29,14 +32,22 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                 <input type="number" placeholder="Valor"/>
 
                 <TransactionTypeContainer>
-                    <button type="button">
+                    <RadioBox 
+                        type="button" 
+                        isActive={type === 'deposit'}
+                        onClick={() => setType('deposit')}
+                    >
                         <img src={incomeImg} alt="Entrada"/>
                         <span>Entrada</span>
-                    </button>
-                    <button type="button">
+                    </RadioBox>
+                    <RadioBox 
+                        type="button" 
+                        isActive={type === 'withdraw'}
+                        onClick={() => setType('withdraw')}
+                    >
                         <img src={outcomeImg} alt="Saída"/>
                         <span>Saída</span>
-                    </button>
+                    </RadioBox>
                 </TransactionTypeContainer>
                 
                 <input placeholder="Categoria"/>
